95ba0dbdbc0dde9b902b8277d8fab8aef6b9228f
Denis Bohm <denis@fireflydesign.com>
2013-05-07 18:03:10 -0500

    merge syncs into a array of non-overlapping spans

--- storage.rb
+++ storage.rb
@@ -36,9 +36,53 @@ class Storage
     db.collection('syncs').update(document, operation, {upsert: true})
   end
 
+  def span_times(s)
+    time = s['time']
+    {time: time , end: time + s['values'].count * s['interval']}
+  end
+
+  def spans_overlap?(a, b)
+    at = span_times(a)
+    bt = span_times(b)
+    (at[:time] <= bt[:end]) && (at[:end] >= bt[:time])
+  end
+
+  def merge_two_spans(a, b)
+    av = a['values']
+    ai = a['interval']
+    ae = a['time'] + av.count * ai
+    bt = b['time']
+    i = (ae - bt) / ai
+    bv = b['values']
+    values = bv[i .. bv.count]
+    av.concat(values) if values
+  end
+
+  def merge_all_spans(spans)
+    spans = spans.sort {|a, b|
+      a['time'] <=> b['time']
+    }
+    index = 0
+    while index < spans.count - 1
+      a = spans[index]
+      b = spans[index + 1]
+      if spans_overlap?(a, b)
+        merge_two_spans(a, b)
+        spans.delete_at(index + 1)
+      else
+        index += 1
+      end
+    end
+    spans
+  end
+
   def query(type, span)
+    result = []
     db = get_connection()
-    db.collection('syncs').find({type => {"$exists" => true}}, fields: {type => 1}).to_a
+    db.collection('syncs').find({type => {'$exists' => true}}, fields: {type => 1}).each {|document|
+      result.concat(document[type])
+    }
+    {type => merge_all_spans(result)}
   end
 
 end
